e1992cb504646d197448eaad5e69bfd3f2e715dc
MatutoDelivery <110774165+MatutoDelivery@users.noreply.github.com>
2022-08-07 21:46:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,47 @@
-const staticCacheName = 'Matuto Delivery'
+// This is the "Offline page" service worker
 
-this.addEventListener('install', event => {
-    this.skipWaiting();
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-    event.waitUntil(
-        caches.open(staticCacheName)
-        .then(cache => {
-            return cache.addAll([
-                'https://matuto-delivery-beta.bubbleapps.io/',
-                'https://matutodelivery.github.io/app/manifest.json',
+const CACHE = "pwabuilder-page";
 
-            ])
-        })
-    )
-})
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
-this.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(cacheName => cacheName.startsWith('Matuto'))
-                .filter(cacheName => cacheName !== staticCacheName)
-                .map(cacheName => caches.delete(cacheName))
-            )
-        })
-    )
-})
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
-this.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request)
-        })
-        .catch(() => {
-            return caches.match('https://matuto-delivery-beta.bubbleapps.io/')
-        })
-    )
-})
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
