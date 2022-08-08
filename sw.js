c134a8ec1ff88a6c9b68bbb0cde769d911af1cd4
MatutoDelivery <110774165+MatutoDelivery@users.noreply.github.com>
2022-08-07 22:37:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var staticCacheName = "Matuto Delivery";
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(["https://matuto-delivery-beta.bubbleapps.io/index"]);
     })
   );
 });
